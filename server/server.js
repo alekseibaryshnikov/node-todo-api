e393a8958c65a53974105d199591d2eaec81f480
Aleksei Baryshnikov <a250188@yandex.ru>
2018-04-09 16:15:28 +0300

    Continue with mongoose

--- server/server.js
+++ server/server.js
@@ -15,10 +15,28 @@ let Todo = mongoose.model('Todo', {
     }
 });
 
-let newTodo = new Todo({
-    text: 'Finish my work!',
-    complited: false,
-    createdAy: 123
+let Users = mongoose.model('Users', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    name: {
+        type: String,
+        trim: true,
+        default: null
+    }
 });
 
-newTodo.save().then(result => console.log(JSON.stringify(result, undefined, 2))).catch(e => console.error(`Unbale to save ${e}`));
+// let newTodo = new Todo({
+//     text: 'Finish my work!',
+//     complited: false,
+//     createdAy: 123
+// });
+
+let newUser = new Users({
+    email: ' a250188@yandex.ru '
+});
+
+newUser.save().then(result => console.log(JSON.stringify(result, undefined, 2))).catch(e => console.error(`Unbale to save ${e}`));
