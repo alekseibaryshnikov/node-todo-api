90016a9bb71770716ce6ac1d96d3aa75920a4ee4
Aleksei Baryshnikov <a250188@yandex.ru>
2018-05-09 22:36:47 +0300

    Added tests for /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -226,7 +226,8 @@ describe('POST /users', () => {
                         expect(user).toBeTruthy();
                         expect(user.password).not.toBe(password);
                         done();
-                    });
+                    })
+                    .catch((e) => done(e));
             });
     });
 
@@ -235,7 +236,7 @@ describe('POST /users', () => {
         let password = '12345';
         request(app)
             .post('/users')
-            .send({email, password})
+            .send({ email, password })
             .expect(400)
             .end(done);
     });
@@ -246,8 +247,38 @@ describe('POST /users', () => {
 
         request(app)
             .post('/users')
-            .send({email, password})
+            .send({ email, password })
             .expect(400)
             .end(done);
+    });
+});
+
+describe('POST /users/login', () => {
+    it ('should return token when user login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toBeTruthy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id)
+                    .then((user) => {
+                        expect(user.tokens[0]).toInclude({
+                            access: 'auth',
+                            token: res.headers['x-auth']
+                        });
+                        done();
+                    })
+                    .catch((e) => done(e));
+            });
     });
 });
